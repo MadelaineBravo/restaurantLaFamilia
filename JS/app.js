3b0a58b35a494abf946df2c232d96d36d315173e
MadelaineBravo <madelainebravo089@gmail.com>
2025-06-24 22:55:12 +0200

    sprint 2

--- JS/app.js
+++ JS/app.js
@@ -1,45 +1,43 @@
 function toggleMenu() {
     const nav = document.querySelector("nav.menu");
     nav.classList.toggle("show");
-  }
-  
-  window.addEventListener("scroll", () => {
+}
+
+window.addEventListener("scroll", () => {
     const header = document.querySelector("header");
     if (window.scrollY > 10) {
-      header.classList.add("transparent");
+        header.classList.add("transparent");
     } else {
-      header.classList.remove("transparent");
+        header.classList.remove("transparent");
     }
-  });
-  
-  
-  function expandImage(img) {
+});
+
+function expandImage(img) {
     const modal = document.getElementById('modal');
     document.getElementById('modalImg').src = img.src;
     modal.classList.add('show');
-  }
-  
-  function closeModal() {
+}
+
+function closeModal() {
     document.getElementById('modal').classList.remove('show');
-  }
-  
+}
+
   // Cambia color header al hacer scroll
-  window.addEventListener("scroll", () => {
+window.addEventListener("scroll", () => {
     const header = document.querySelector("header");
     if (window.scrollY > 10) {
-      header.classList.add("transparent");
+        header.classList.add("transparent");
     } else {
-      header.classList.remove("transparent");
+        header.classList.remove("transparent");
     }
-  });
-  const boton = document.getElementById('hamburguesa');
-  const menu = document.getElementById('menu');
-  const overlay = document.getElementById('overlay');
-  boton.addEventListener('click', () => {
-   menu.classList.toggle('activo');
-   overlay.classList.toggle('activo');
-  });
-  overlay.addEventListener('click', () => {
-   menu.classList.remove('activo');
-   overlay.classList.remove('activo');});
-  
+});
+    const boton = document.getElementById('hamburguesa');
+    const menu = document.getElementById('menu');
+    const overlay = document.getElementById('overlay');
+boton.addEventListener('click', () => {
+    menu.classList.toggle('activo');
+    overlay.classList.toggle('activo');
+});
+    overlay.addEventListener('click', () => {
+menu.classList.remove('activo');
+overlay.classList.remove('activo');});
